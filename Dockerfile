6cc4731010b2a2a8b4990a2681eb92be8f979f7e
Nicolas Fischer <nicolas@emberspark.io>
2019-09-18 16:52:22 +0100

    version 7.13.5 - back to using Atlassian's Docker images

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,18 @@
-FROM dchevell/jira-software:7.5.2
+FROM atlassian/jira-software:7.13.5
 
 # The id of the jira user is 2001
 RUN chown -R 2001:2001 /opt/atlassian
 
-RUN touch /etc/container_id && \
-    chown 2001:2001 /etc/container_id
+COPY bin/hardening.py /hardening.py
+# modify the original entrypoint.py to call our hardening functions
+RUN sed -i '/import jinja2.*/a from hardening import gen_cfg_no_chown' /entrypoint.py && \
+    sed -i '/import jinja2.*/a from hardening import all_logs_to_stdout' /entrypoint.py && \
+    sed -i '/os.execv/i all_logs_to_stdout()' /entrypoint.py && \
+    sed -i 's/^gen_cfg/gen_cfg_no_chown/' /entrypoint.py
 
-COPY bin/entrypoint.sh /entrypoint.sh
-RUN chmod +x /entrypoint.sh
+# Atlassian's gen_cfg and our gen_cfg_no_chown should have the same signature
+# Fail the build if that's not the case
+COPY bin/check_signatures.sh /
+RUN chmod +x /check_signatures.sh && /check_signatures.sh
 
-CMD ["/bin/sh", "-c", "ATL_JDBC_URL=jdbc:${JIRA_JDBC_URL_DRIVER}://${JIRA_DB_ENDPOINT}:${JIRA_DB_PORT}/${JIRA_DB_NAME} /entrypoint.sh -fg"]
+CMD ["/bin/sh", "-c", "ATL_JDBC_URL=jdbc:${JIRA_JDBC_URL_DRIVER}://${JIRA_DB_ENDPOINT}:${JIRA_DB_PORT}/${JIRA_DB_NAME} /entrypoint.py -fg"]
---
+++ bin/hardening.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+import logging
+import os
+import time
+import jinja2 as j2
+from pwd import getpwuid
+
+
+# Find owner of a file
+def get_owner(filename):
+    return getpwuid(os.stat(filename).st_uid).pw_name
+
+# Get the logs symlinked to stdout
+def symlink_log(log_file):
+        if not os.path.islink(log_file):
+                if os.path.exists(log_file):
+                        os.rename(log_file, f"{log_file}.{time.strftime('%Y-%m-%d-%H-%M-%S', time.gmtime())}")
+                os.symlink('/dev/stdout', log_file)
+
+# generate config file, defaulting to run user and group, not root, and allowing to skip permission setting
+def gen_cfg_no_chown(tmpl, target, env, user='root', group='root', mode=0o644, overwrite=True):
+    if not overwrite and os.path.exists(target):
+        logging.info(f"{target} exists; skipping.")
+        return
+    logging.info(f"Generating {target} from template {tmpl} (no chown)")
+    # Setup Jinja2 for templating
+    jenv = j2.Environment(
+        loader=j2.FileSystemLoader('/opt/atlassian/etc/'),
+        autoescape=j2.select_autoescape(['xml']))
+    cfg = jenv.get_template(tmpl).render(env)
+    with open(target, 'w') as fd:
+        fd.write(cfg)
+    if get_owner(target) == 'root':
+        logging.info(f"Owner of {target} is {get_owner(target)}; not attempting to change permissions")
+    else:
+        os.chmod(target, mode)
+    # ignore user and group
+
+# Get the Jira logs out to stdout
+def all_logs_to_stdout():
+    logging.info(f"Generating symlinks to stdout for logs")
+    logs_folder = f"{os.environ['JIRA_HOME']}/log"
+    if not os.path.exists(logs_folder):
+        os.makedirs(logs_folder)
+    symlink_log(f"{logs_folder}/atlassian-greenhopper.log")
+    symlink_log(f"{logs_folder}/atlassian-jira.log")
+    symlink_log(f"{logs_folder}/atlassian-jira-apdex.log")
+    symlink_log(f"{logs_folder}/atlassian-jira-incoming-mail.log")
+    symlink_log(f"{logs_folder}/atlassian-jira-security.log")
+    symlink_log(f"{logs_folder}/atlassian-jira-slow-queries.log")
+    symlink_log(f"{logs_folder}/atlassian-servicedesk.log")
