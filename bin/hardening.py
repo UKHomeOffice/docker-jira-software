e5b3433ee8ed1ec972a16df237731bad658bea9f
Kit Baker <bakerk@DAS00944.local>
2020-02-27 14:16:06 +0000

    patching image7.13.12-1 to 7.13.12-2

--- bin/hardening.py
+++ bin/hardening.py
@@ -20,11 +20,11 @@ def get_owner(filename):
     return getpwuid(os.stat(filename).st_uid).pw_name
 
 # Get the logs symlinked to stdout
-def symlink_log(log_file):
-        if not os.path.islink(log_file):
-                if os.path.exists(log_file):
-                        os.rename(log_file, f"{log_file}.{time.strftime('%Y-%m-%d-%H-%M-%S', time.gmtime())}")
-                os.symlink('/dev/stdout', log_file)
+# def symlink_log(log_file):
+#         if not os.path.islink(log_file):
+#                 if os.path.exists(log_file):
+#                         os.rename(log_file, f"{log_file}.{time.strftime('%Y-%m-%d-%H-%M-%S', time.gmtime())}")
+#                 os.symlink('/dev/stdout', log_file)
 
 # generate config file, defaulting to run user and group, not root, and allowing to skip permission setting
 def gen_cfg_no_chown(tmpl, target, user='root', group='root', mode=0o644, overwrite=True):
@@ -44,15 +44,15 @@ def gen_cfg_no_chown(tmpl, target, user='root', group='root', mode=0o644, overwr
     #     set_perms(target, user, group, mode)
 
 # Get the Jira logs out to stdout
-def all_logs_to_stdout():
-    logging.info(f"Generating symlinks to stdout for logs")
-    logs_folder = f"{os.environ['JIRA_HOME']}/log"
-    if not os.path.exists(logs_folder):
-        os.makedirs(logs_folder)
-    symlink_log(f"{logs_folder}/atlassian-greenhopper.log")
-    symlink_log(f"{logs_folder}/atlassian-jira.log")
-    symlink_log(f"{logs_folder}/atlassian-jira-apdex.log")
-    symlink_log(f"{logs_folder}/atlassian-jira-incoming-mail.log")
-    symlink_log(f"{logs_folder}/atlassian-jira-security.log")
-    symlink_log(f"{logs_folder}/atlassian-jira-slow-queries.log")
-    symlink_log(f"{logs_folder}/atlassian-servicedesk.log")
+# def all_logs_to_stdout():
+#     logging.info(f"Generating symlinks to stdout for logs")
+#     logs_folder = f"{os.environ['JIRA_HOME']}/log"
+#     if not os.path.exists(logs_folder):
+#         os.makedirs(logs_folder)
+#     symlink_log(f"{logs_folder}/atlassian-greenhopper.log")
+#     symlink_log(f"{logs_folder}/atlassian-jira.log")
+#     symlink_log(f"{logs_folder}/atlassian-jira-apdex.log")
+#     symlink_log(f"{logs_folder}/atlassian-jira-incoming-mail.log")
+#     symlink_log(f"{logs_folder}/atlassian-jira-security.log")
+#     symlink_log(f"{logs_folder}/atlassian-jira-slow-queries.log")
+#     symlink_log(f"{logs_folder}/atlassian-servicedesk.log")
